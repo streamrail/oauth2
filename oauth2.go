d59b4b4591f327c4981a5aa3046608a411e3774e
Shay Erlichmen <erlichmen@gmail.com>
2016-03-24 16:02:44 +0200

    Update oauth2.go

--- oauth2.go
+++ oauth2.go
@@ -5,7 +5,7 @@
 // Package oauth2 provides support for making
 // OAuth2 authorized and authenticated HTTP requests.
 // It can additionally grant authorization with Bearer JWT.
-package oauth2 // import "golang.org/x/oauth2"
+package oauth2
 
 import (
 	"bytes"
@@ -16,7 +16,7 @@ import (
 	"sync"
 
 	"golang.org/x/net/context"
-	"golang.org/x/oauth2/internal"
+	"github.com/streamrail/oauth2/internal"
 )
 
 // NoContext is the default context you should supply if not using
